87b32b35c3b69e22b06cc4fb0a1aacf531589480
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-05 19:17:58 -0300

    Finishes Styling Form

--- src/app/components/simulacao/simulacao.component.ts
+++ src/app/components/simulacao/simulacao.component.ts
@@ -8,17 +8,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class SimulacaoComponent implements OnInit {
 
-  
-  simuladorForm = this.fb.group({
-    ddd_origin: ['ave'],
-    ddd_destiny: [''],
+  public isPlanSelected:boolean = false;
+  public simuladorForm = this.fb.group({
+    ddd_origin: ['1'],
+    ddd_destiny: ['2'],
     time: ['15'],
     planId: [''],
   });
-  
+
   constructor(private fb: FormBuilder) { }
   
-
   ngOnInit() {
   }
 
@@ -28,6 +27,20 @@ export class SimulacaoComponent implements OnInit {
                 this.simuladorForm.get('ddd_destiny').value, 
                 this.simuladorForm.get('time').value, 
                 this.simuladorForm.get('planId').value)  
+  }
+
+  planSelected(event) {
+    this.isPlanSelected = true;
+    var selectedPlan = event.path[1];
+    var plans = event.path[2].childNodes;
+
+    plans.forEach(plan => {
+      plan == selectedPlan ? 
+        plan.setAttribute("style", "background-color: var(--PRIMARY-COLOR); color: white; border-color: var(--PRIMARY-COLOR); font-weight: 500") : 
+        plan.setAttribute("style", "background-color: white; color: black; border-color: black; font-weight: 100")
+    });
+
+    this.update()
   }
 
 }
