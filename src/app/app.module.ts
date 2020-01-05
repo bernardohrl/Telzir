bff932bfdf7403e7a3413af522726b173d361034
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-04 21:46:26 -0300

    Generates Simulacao Component
    Also includes bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SimulacaoComponent } from './components/simulacao/simulacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimulacaoComponent
   ],
   imports: [
     BrowserModule
